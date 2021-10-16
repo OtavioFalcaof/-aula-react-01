29f015feee58c16093de7d28712686b31a7f1d30
Otavio Augusto <otavio_falcao654@hotmail.com>
2021-10-16 13:35:25 -0300

    React - aula 01

---
+++ ola-mundo/src/App.js
@@ -0,0 +1,17 @@
+import logo from './componentes/imgs/Coruja-buraqueira.jpg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+      <img src={logo} />
+      <pre>Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria, tinha uma coruja como mascote.<br/> 
+        Os gregos consideravam a noite como o momento do pensamento filosófico e da revelação intelectual<br/>
+         e a coruja, por ser uma ave noturna, acabou representando essa busca pelo saber.</pre>
+         </div>
+  );
+}
+
+export default App;
